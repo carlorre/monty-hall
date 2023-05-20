7f16c95a9d9209445d743ec9bf89d65994b66871
mhCalle <carl.orre@medhelp.se>
2023-05-20 16:42:45 +0200

    Created input components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  const [inputValue, setInputValue] = useState("");
+  const [changeDoor, setChangeDoor] = useState(false);
+
+  const handleSubmit = () => {
+    console.log(inputValue, changeDoor);
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center">
+      <h1>Monty Hall Simulation</h1>
+      <form className="flex flex-col" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="numberInput">Number of Simulations:</label>
+          <input
+            id="numberInput"
+            type="number"
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="changeDoor">Change door</label>
+          <input
+            id="changeDoor"
+            type="checkbox"
+            onChange={() => setChangeDoor(!changeDoor)}
+          />
+        </div>
+        <button type="submit">Simulate</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
